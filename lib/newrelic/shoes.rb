a3d8a6d9c18c05041be42249fcce227a729b9619
Jason R. Clark <jclark@newrelic.com>
2015-04-07 22:55:04 -0700

    Trace painting methods

--- lib/newrelic/shoes.rb
+++ lib/newrelic/shoes.rb
@@ -12,12 +12,34 @@ Shoes::InternalApp.add_global_keypress(:'control_alt_n') do
 
   class Shoes::Swt::Common::Painter
     include NewRelic::Agent::MethodTracer
+
     add_method_tracer :paint_control, 'Shoes/#{self.class.name}/paint_control'
+    add_method_tracer :paint_object,  'Shoes/#{self.class.name}/paint_object'
+
+    add_method_tracer :fill_setup, 'Shoes/#{self.class.name}/fill_setup'
+    add_method_tracer :draw_setup, 'Shoes/#{self.class.name}/draw_setup'
+    add_method_tracer :set_rotate, 'Shoes/#{self.class.name}/set_rotate'
+  end
+
+  class Shoes::Swt::RectPainter
+    include NewRelic::Agent::MethodTracer
+
+    add_method_tracer :fill, 'Shoes/#{self.class.name}/fill'
+    add_method_tracer :draw, 'Shoes/#{self.class.name}/draw'
   end
 
   class Shoes::Swt::TextBlock::Painter
     include NewRelic::Agent::MethodTracer
+
     add_method_tracer :paintControl, 'Shoes/#{self.class.name}/paint_control'
+    add_method_tracer :draw_layouts, 'Shoes/#{self.class.name}/draw_layouts'
+  end
+
+  class Shoes::Swt::ColorFactory
+    include NewRelic::Agent::MethodTracer
+
+    add_method_tracer :create, 'Shoes/#{self.class.name}/create'
+    add_method_tracer :create_new, 'Shoes/#{self.class.name}/create_new'
   end
 
   at_exit do
