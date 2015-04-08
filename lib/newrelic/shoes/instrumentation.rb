52d6066a6c7c2e7fcd26365f98c98aea641edcc9
Jason R. Clark <jclark@newrelic.com>
2015-04-07 23:18:34 -0700

    Meta-up the trace applications

--- lib/newrelic/shoes/instrumentation.rb
+++ lib/newrelic/shoes/instrumentation.rb
@@ -7,36 +7,26 @@ module NewRelic
       def self.install
         NewRelic::Agent.manual_start(monitor_mode: false, developer: true)
 
-        ::Shoes::Swt::Common::Painter.class_eval do
+        trace(::Shoes::Swt::Common::Painter, :paint_control)
+        trace(::Shoes::Swt::Common::Painter, :paint_object)
+        trace(::Shoes::Swt::Common::Painter, :fill_setup)
+        trace(::Shoes::Swt::Common::Painter, :draw_setup)
+        trace(::Shoes::Swt::Common::Painter, :set_rotate)
+
+        trace(::Shoes::Swt::RectPainter, :fill)
+        trace(::Shoes::Swt::RectPainter, :draw)
+
+        trace(::Shoes::Swt::TextBlock::Painter, :paintControl)
+        trace(::Shoes::Swt::TextBlock::Painter, :draw_layouts)
+
+        trace(::Shoes::Swt::ColorFactory, :create)
+        trace(::Shoes::Swt::ColorFactory, :create_new)
+      end
+
+      def self.trace(clazz, method_name, metric_suffix=method_name)
+        clazz.class_eval do
           include NewRelic::Agent::MethodTracer
-
-          add_method_tracer :paint_control, 'Shoes/#{self.class.name}/paint_control'
-          add_method_tracer :paint_object,  'Shoes/#{self.class.name}/paint_object'
-
-          add_method_tracer :fill_setup, 'Shoes/#{self.class.name}/fill_setup'
-          add_method_tracer :draw_setup, 'Shoes/#{self.class.name}/draw_setup'
-          add_method_tracer :set_rotate, 'Shoes/#{self.class.name}/set_rotate'
-        end
-
-        ::Shoes::Swt::RectPainter.class_eval do
-          include NewRelic::Agent::MethodTracer
-
-          add_method_tracer :fill, 'Shoes/#{self.class.name}/fill'
-          add_method_tracer :draw, 'Shoes/#{self.class.name}/draw'
-        end
-
-        ::Shoes::Swt::TextBlock::Painter.class_eval do
-          include NewRelic::Agent::MethodTracer
-
-          add_method_tracer :paintControl, 'Shoes/#{self.class.name}/paint_control'
-          add_method_tracer :draw_layouts, 'Shoes/#{self.class.name}/draw_layouts'
-        end
-
-        ::Shoes::Swt::ColorFactory.class_eval do
-          include NewRelic::Agent::MethodTracer
-
-          add_method_tracer :create, 'Shoes/#{self.class.name}/create'
-          add_method_tracer :create_new, 'Shoes/#{self.class.name}/create_new'
+          add_method_tracer method_name, 'Shoes/#{self.class.name}/' + metric_suffix.to_s
         end
       end
     end
